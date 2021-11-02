733357d9674ea1fbddcc53e216bcdf5ba4eee832
Piotr <piotrsenkow@gmail.com>
2021-11-02 01:04:03 -0500

    adding formatting changes

---
+++ src/pages/faq.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Faq = () => (
+  <Layout>
+    <SEO title="FAQ" />
+    <p>Hello</p>
+  </Layout>
+)
+
+export default Faq
