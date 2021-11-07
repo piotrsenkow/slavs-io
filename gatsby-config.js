722af0a2db8b32461dc71bf1f7a448f52155dc03
Piotr <piotrsenkow@gmail.com>
2021-11-07 11:38:52 -0600

    robots crawlers

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,45 @@
 module.exports = {
   siteMetadata: {
     title: `Squatting Slavs of Solana`,
-    siteUrl: `https://www.example.com`,
+    siteUrl: `https://www.slavs.io`,
     description: `Squatting Slavs of Solana is an NFT collection arriving to the Solana blockchain December 2021 in the form of 10,000, unique, randomly-generated, Squatting Slavs originating from the 13 slavic countries.`,
-    author: `@piotrsenkow`,
+    author: `@slavicnfts`,
   },
   flags: {
     THE_FLAG: false
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-212212122-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "slavs.io",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
